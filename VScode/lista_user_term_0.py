b9581e762146003fcefb9ab722d80ec05d0358a1
cey <ceypheron@gmail.com>
2022-08-21 09:42:13 -0300

    list2

--- VScode/lista_user_term_0.py
+++ VScode/lista_user_term_0.py
@@ -2,3 +2,12 @@
 
 
 lista = []
+
+while lista.append() != 0:
+    lista.append(input('Qual valor deseja adicionar a lista?\n'))
+
+tam = len(lista) - 1
+
+while tam >= 0:
+    print(lista[tam], end=", ")
+    tam = tam - 1

f6f634dc1b75f22091538df72acf53fb057314cd
cey <ceypheron@gmail.com>
2022-08-21 09:23:17 -0300

    list

---
+++ VScode/lista_user_term_0.py
@@ -0,0 +1,4 @@
+#usuario digita a lista com final 0 e depois a lista e impressa ao contrario
+
+
+lista = []
--- VScode/listas.py
+++ VScode/listas.py
@@ -2,3 +2,10 @@
 
 playlist = ['Scrim','Nirvana','BONES','White Buffalo','Hippie Sabotage']
 
+print('Lista atual: {}'.format(playlist))
+
+alterar = int(input('Deseja realizar alguma alteração?\n1 para sim\n2 para nao'))
+
+if alterar == 1:
+    playlist.append(input('O que deseja adicionar na playlist? '))
+    print('Sua lista ficou:\n{}'.format(playlist))

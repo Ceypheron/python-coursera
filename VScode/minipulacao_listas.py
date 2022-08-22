b16ce5637e15399d72ba3c277544d8dcfc36e7af
cey <ceypheron@gmail.com>
2022-08-22 10:14:41 -0300

    manipulacao lista ok

---
+++ VScode/minipulacao_listas.py
@@ -0,0 +1,35 @@
+#minipulacao de listas
+
+playlist = ['nirvana','bones','scrim','brennan savage']
+playlist_2 = [1,2,3,4,5]
+#print(playlist)
+
+#CLONAR LISTA  >>>>>       lista2 = lista1[:]
+"""def clone (playlist):
+    clone = playlist[:]
+    return clone"""
+#--------------------------------------------------------------------
+##pertinencia a uma lista
+
+"""if 'nirvana' in playlist:
+    print("Nirvana esta na playlist.")
+else:
+    print('Nirvana nao esta na playlisy.')"""
+#---------------------------------------------------------------------
+##concatenar lista
+
+#print([1,2,3]+[4,5,6])
+#print(playlist + playlist_2)
+#----------------------------------------------------------------------
+#repeticao de lista
+
+#playlist_triplicada = playlist * 3
+#print(playlist_triplicada)
+#----------------------------------------------------------------------
+##remocao de elementos em listas
+
+"""del playlist_2[0]
+print(playlist_2)
+
+del playlist[0:3]
+print(playlist)"""
